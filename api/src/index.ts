e711465fb263307757a576ad2283e4b01f4ad480
Roman Silivestrov <roman.silivestrov@metalamp.io>
2023-05-03 23:04:06 +0300

    Connect api to the database

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,17 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { Client } from 'pg';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const client = new Client();
+
+client
+  .connect()
+  .then(() => console.log('⚡️[server]: Connected to database'))
+  .catch((err) => console.error('⚡️[server]: Database connection error', err.message));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
